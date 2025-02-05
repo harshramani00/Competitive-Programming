65895eb731f54a75b197eca9601a7834e2288d9a
Harsh Anilkumar Ramani <143819834+harshramani00@users.noreply.github.com>
2025-02-05 09:19:02 -0500

    Create 242.Valid_Anagram.py

---
+++ leetcode/242.Valid_Anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if sorted(s) == sorted(t):
+            return True
+        else: return False
+
+# Time complexity of sorted is O(nlog(n))
