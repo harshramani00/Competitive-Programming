239bb7eb2bc53a6f82f6e4611be99b85d50179af
Harsh Anilkumar Ramani <143819834+harshramani00@users.noreply.github.com>
2025-02-15 22:05:21 -0500

    Update 121.Best_Time_to_Buy_and_Sell_Stock.py

--- leetcode/121.Best_Time_to_Buy_and_Sell_Stock.py
+++ leetcode/121.Best_Time_to_Buy_and_Sell_Stock.py
@@ -11,3 +11,22 @@ class Solution(object):
             profit = price - min_price
             prf = max(profit,prf)
         return prf
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = 1
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+# Time complexity is O(n)
